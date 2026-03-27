6d52f745ed5b8ebd4a2a5fd77b3fcbd9dc99b5da
Brindha-log <brindhaloganathan0@gmail.com>
2026-03-27 13:31:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// PRODUCT DATA
+const products = [
+  { id: 1, name: "Burger", price: 120, image: "assets/burger.jpg" },
+  { id: 2, name: "Pizza", price: 250, image: "assets/pizza.jpg" },
+  { id: 3, name: "Pasta", price: 180, image: "assets/pasta.jpg" },
+  { id: 4, name: "Sandwich", price: 100, image: "assets/sandwich.jpg" }
+];
+
